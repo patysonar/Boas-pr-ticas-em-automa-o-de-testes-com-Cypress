433d52c182be21eae7193ea860e3a6114231c32d
Patricia <patysonar@gmail.com>
2022-03-22 18:18:58 -0300

    Aula Hardcoded - Complexidade desnecessária.

--- cypress/integration/unnecessary-complexity/sample.spec.js
+++ cypress/integration/unnecessary-complexity/sample.spec.js
@@ -2,26 +2,19 @@ describe('Unnecessary complexity anti-patter', () => {
   beforeEach(() => {
     cy.visit('https://bit.ly/2XSuwCW')
 
-    if (Math.random() > 0.5) {
+    if (Math.random() > 0.5) {  //seleciona o checkbox
       cy.get('#agree')
         .click()
+      
     }
   })
 
-  Cypress._.times(5, () => {
-    it('checks the checkbox only if not checked', () => {
-      cy.get('body').then($body => {
-        if ($body.find('#agree:checked').length) {
-          cy.log('check box was checked')
-          return
-        }
-        cy.log('check box was unchecked')
-        $body.find('#agree').click()
-        return
-      })
-
+  Cypress._.times(5, () => {  //Roda o teste 5 vezes
+    it('checks the checkbox only if not checked', () => { // marca se não tiver marcado o checkbox
       cy.get('#agree')
+        .check()
         .should('be.checked')
     })
   })
 })
+
